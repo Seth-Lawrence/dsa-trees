4c8378530d21de1e42e2cb0f955e4d5297c9a324
Nicholas <nick.orsi1992@gmail.com>
2024-02-28 12:27:31 -0800

    Solved countEvens problem.

--- trees/count-evens/countEvens.ts
+++ trees/count-evens/countEvens.ts
@@ -4,7 +4,23 @@ import { TreeNodeNum } from "../common/tree";
  * children, count how many nodes have even values. Returns that count as
  * an integer. */
 function countEvens(node: TreeNodeNum): number {
-  return 42;
+  // Use a stack (array) to gather nodes
+  const stack: TreeNodeNum[] = [node];
+  // Define evenCount variable
+  let count: number = 0;
+  // Use loop to traverse depth first search
+  while(stack.length !== 0) {
+    // Define current value as popped off node
+    const current: TreeNodeNum = stack.pop()!;
+    // If value % 2 === 0 increment count
+    if(current.val % 2 === 0) count++;
+    // Loop through chrildren, add to stack
+    for(const child of current.children) {
+      stack.push(child);
+    }
+  }
+// return count
+  return count;
 }
 
 export { countEvens };
