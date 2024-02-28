aacdbfc2259b985fb591620e6b9b0728414a3236
Seth Hobson <sethlawrencehobson@gmail.com>
2024-02-28 15:10:36 -0500

    finished sumValues

--- trees/sum-values/sumValues.ts
+++ trees/sum-values/sumValues.ts
@@ -3,7 +3,18 @@ import { TreeNodeNum } from "../common/tree";
 /** sumValues(): add up all values of node and its descendants.
  * Returns sum as an integer. */
 function sumValues(node: TreeNodeNum): number {
-  return 42;
+  const stack:TreeNodeNum[] = [node];
+  let sum = 0;
+
+  while (!(stack.length === 0)) {
+    const current = stack.pop();
+    sum += current!.val
+
+    for (let child of current!.children) {
+      stack.push(child)
+    }
+  }
+  return sum;
 }
 
 export { sumValues };
