31033986dd70e2a474812b5def4662bdd316a468
Nicholas <nick.orsi1992@gmail.com>
2024-02-29 11:37:21 -0800

    Added a test for another long tree to ensure maxdepth working correctly.

--- trees/common/tree.test.ts
+++ trees/common/tree.test.ts
@@ -36,9 +36,47 @@ const largeTree = new Tree(
       ]),
     ]));
 
+
+//            1
+//          / | \
+//        2   3   4
+//       / \     / \
+//      9  10   5   6
+//           \        \
+//           11        7
+//             \        \
+//             12        8
+//            /  \
+//          13    14
+
+const largeTree2 = new Tree(
+    new TreeNodeNum(1, [
+      new TreeNodeNum(2, [
+        new TreeNodeNum(9),
+        new TreeNodeNum(10, [
+          new TreeNodeNum(11, [
+            new TreeNodeNum(12, [
+              new TreeNodeNum(13),
+              new TreeNodeNum(14)
+            ])
+          ])
+        ])
+      ]),
+      new TreeNodeNum(3),
+      new TreeNodeNum(4, [
+        new TreeNodeNum(5),
+        new TreeNodeNum(6, [
+          new TreeNodeNum(7, [
+            new TreeNodeNum(8),
+          ]),
+        ]),
+      ]),
+    ]));
+
 it("can create trees", function () {
   expect(largeTree.root!.val).toBe(1);
+  expect(largeTree2.root!.val).toBe(1);
   expect(smallTree.root!.val).toBe(1);
 });
 
-export { smallTree, largeTree };
+export { smallTree, largeTree, largeTree2 };
