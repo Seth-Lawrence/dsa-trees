672973e5d6fd31f0cf677c458bd8c39e036ad2bd
Nicholas <nick.orsi1992@gmail.com>
2024-02-29 11:59:30 -0800

    Removed code no longer used.

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -7,39 +7,17 @@ import { TreeNodeNum } from "../common/tree";
 
 function maxDepth(node: TreeNodeNum | null): number {
   if (node === null) return 0;
-  // debugger;
   let branchMaxDepth: number = 1;
   let currentDepth: number = 1;
-  // let max: number = 1;
-  debugger
+
   if (node.children.length > 0) {
     for (let child of node.children) {
       const branchDepth = currentDepth + maxDepth(child);
-      // max = depth > max ? depth : max;
       branchMaxDepth = branchDepth > branchMaxDepth ? branchDepth : branchMaxDepth;
     }
   }
 
-  // return max;
   return branchMaxDepth;
-
-
-  // const stack:TreeNodeNum[] = [node];
-  // let depth = 0;
-  // let max = 0;
-
-  // while (!(stack.length === 0)) {
-  //   const current = stack.pop();
-  //   depth++
-
-  //   for (let child of current!.children) {
-  //     stack.push(child)
-  //     depth++
-  //   }
-  //   max = depth > max ? depth : max;
-  //   depth = 0;
-  // }
-  // return max;
 }
 
 export { maxDepth };
