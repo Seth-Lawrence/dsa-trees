60fdeb9695642938480ca9f818de986a1e20fe92
Seth Hobson <sethlawrencehobson@gmail.com>
2024-02-28 20:00:25 -0500

    debugging maxDepth, passed first test, failed second

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -6,7 +6,45 @@ import { TreeNodeNum } from "../common/tree";
  **/
 
 function maxDepth(node: TreeNodeNum | null): number {
-  return 42;
+  if (node === null) return 0;
+  // debugger;
+  let depth: number = 1;
+  let max: number = 1;
+
+  if (node.children.length > 0) {
+    for (let child of node.children) {
+      maxDepth(child);
+      if (child.children) depth++;
+      max = depth > max ? depth : max;
+    }
+  }
+
+  return max;
+
+
+
+  // const stack:TreeNodeNum[] = [node];
+  // let depth = 0;
+  // let max = 0;
+
+  // while (!(stack.length === 0)) {
+  //   const current = stack.pop();
+  //   depth++
+
+  //   for (let child of current!.children) {
+  //     stack.push(child)
+  //     depth++
+  //   }
+  //   max = depth > max ? depth : max;
+  //   depth = 0;
+  // }
+  // return max;
 }
 
 export { maxDepth };
+
+
+// does node have children
+// increase depth +1
+//iterate through children
+//
