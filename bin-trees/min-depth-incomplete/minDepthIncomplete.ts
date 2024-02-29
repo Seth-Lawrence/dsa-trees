8b460a89807f19dd5a473ae40018e4f3e412ad5c
Nicholas <nick.orsi1992@gmail.com>
2024-02-29 13:59:02 -0800

    Added psuedo code.

--- bin-trees/min-depth-incomplete/minDepthIncomplete.ts
+++ bin-trees/min-depth-incomplete/minDepthIncomplete.ts
@@ -1,12 +1,18 @@
 import { BNodeNum } from "../common/bintree";
-
+import {QueueNum} from "../common/queue";
 /** Minimum depth from node to an "incomplete node".
  *
  * An incomplete node is a node with 0 or 1 children (not 2).
  */
 
 function minDepthToIncompleteNode(node: BNodeNum): number {
-  return 42;
+  //Create queue
+  //Create currentDepthQueueLine = 1
+  //Create nextDepthQueueLine =
+  //Create depth
+  //Create while loop, run as long as queue not empty
+    //Decrement currentqueueLine
+
 }
 
 export { minDepthToIncompleteNode };
